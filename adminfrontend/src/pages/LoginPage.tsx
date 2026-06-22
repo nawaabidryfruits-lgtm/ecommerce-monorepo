60c9b4191e7de417436e16664e068fe8ca545e5c
nawaabidryfruits-lgtm <nawaabidryfruits@gmail.com>
2026-06-22 22:52:20 +0400

    Update LoginPage.tsx

--- adminfrontend/src/pages/LoginPage.tsx
+++ adminfrontend/src/pages/LoginPage.tsx
@@ -25,7 +25,7 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLoginSuccess }) => {
 
     setIsLoading(true);
     try {
-      const response = await fetch('/siteconfig-api/admin-auth/login', {
+      const response = await fetch('https://ecommerce-monorepo-kzb8.onrender.com/api/admin-auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
